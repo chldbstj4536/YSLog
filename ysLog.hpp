98214f8c2fa1823c9cf83d273cc079ca28399369
chldbstj4536 <chldbstj4536@gmail.com>
2023-06-07 23:07:56 +0900

    source_location을 사용해 log 함수 구현

---
+++ ysLog.hpp
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include <iostream>
+#include <format>
+#include <string_view>
+#include <source_location>
+
+namespace YS::Log
+{
+    void log(const std::string_view message, const std::ostream& outStream = std::cout , const std::source_location& location = std::source_location::current())
+    {
+        outStream << std::format("{}\n:filename : {}\nline : {}\n", message, location.file_name(), location.line());
+    }
+}
